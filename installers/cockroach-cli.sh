9a8816e29d0b3d42b284277cb1829ed3811c7239
Antonio Lobato <aj.lobato@gmail.com>
2021-07-18 05:03:28 +0000

    added cockroach-db cli

---
+++ installers/cockroach-cli.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export COCKROACH_VERSION=21.1.5
+
+rm -rf /home/${USER}/.apps/cdb
+mkdir -p /home/${USER}/.apps
+mkdir -p /home/${USER}/.local/bin
+cd /tmp
+
+wget -q -O /tmp/cdb.tgz \
+  https://binaries.cockroachdb.com/cockroach-v${COCKROACH_VERSION}.linux-amd64.tgz && \
+  tar -xzf /tmp/cdb.tgz && \
+  mv cockroach-v${COCKROACH_VERSION}.linux-amd64 /home/${USER}/.apps/cdb && \
+  ln -fs /home/${USER}/.apps/cdb/cockroach /home/${USER}/.local/bin/cockroach && \
+  rm -rf /tmp/cdb.tgz
